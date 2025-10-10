12002c0d71e4fbe5c7b1ff1d65480a13edb01a48
Vinci Velasco <vincivelasco@gmail.com>
2025-10-10 13:46:19 -0700

    initial commit

---
+++ src/main/java/com/example/Vulnerable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// command line injection vulnerable class
+public class Vulnerable {
+    public static void main(String[] args) throws Exception {
+        Scanner myObj = new Scanner(System.in);
+        // potential source
+        String userInput = myObj.nextLine();
+        String cmd = "./mytool --version " + userInput;
+
+        // potential sink
+        Runtime.getRuntime().exec(cmd);
+    }
+}
